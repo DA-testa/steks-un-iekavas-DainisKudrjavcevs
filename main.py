93c8f82bf4a9a94380208bede14ff7dd5c49c1eb
DainisKudrjavcevs <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-02-17 16:03:25 +0200

    ()()()

--- main.py
+++ main.py
@@ -13,18 +13,30 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(Bracket(next, i))
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+            last_opening_bracket = opening_brackets_stack.pop()
+            if not are_matching(last_opening_bracket.char, next):
+                return i + 1
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack[0].position + 1
+    return "Success"
 
 def main():
-    text = input()
+    input_type = input("Choose input type (F for file, I for input): ")
+    if input_type == "F":
+        filename = input("Enter filename: ")
+        with open(filename, "r") as file:
+            text = file.read().strip()
+    elif input_type == "I":
+        text = input("Enter brackets: ").strip()
+    else:
+        print("Invalid input type")
+        return
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
